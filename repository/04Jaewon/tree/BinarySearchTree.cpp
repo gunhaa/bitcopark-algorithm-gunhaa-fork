c46a8d4cbf542060dd416a002ca5615a6845312e
jaewon <jw140@naver.com>
2025-03-08 22:14:49 +0900

    feat: 이진 탐색 트리에 노드 제거 기능 추가

--- repository/04Jaewon/tree/BinarySearchTree.cpp
+++ repository/04Jaewon/tree/BinarySearchTree.cpp
@@ -24,6 +24,11 @@ public:
         insert(root, value);
     }
 
+    void remove(int value)
+    {
+        root = remove(root, value);
+    }
+
     void inorder()
     {
         inorder(root);
@@ -44,6 +49,63 @@ private:
         {
             insert(node->right, value);
         }
+    }
+
+    Node *remove(Node *node, int value)
+    {
+        if (node == nullptr)
+            return node;
+
+        if (value < node->data)
+        {
+            node->left = remove(node->left, value);
+        }
+
+        else if (value > node->data)
+        {
+            node->right = remove(node->right, value);
+        }
+        
+        else
+        {
+            if (node->left == nullptr && node->right == nullptr)
+            {
+                delete node;
+                return nullptr;
+            }
+
+            else if (node->left == nullptr)
+            {
+                Node *temp = node->right;
+                delete node;
+                return temp;
+            }
+            
+            else if (node->right == nullptr)
+            {
+                Node *temp = node->left;
+                delete node;
+                return temp;
+            }
+            
+            else
+            {
+                Node *min_node = find_min(node->right);
+                node->data = min_node->data;
+                node->right = remove(node->right, min_node->data);
+            }
+        }
+
+        return node;
+    }
+
+    Node *find_min(Node *node)
+    {
+        while (node != nullptr && node->left != nullptr)
+        {
+            node = node->left;
+        }
+        return node;
     }
 
     void inorder(Node *node)
@@ -67,6 +129,12 @@ int main()
     bst.insert(70);
     bst.insert(60);
     bst.insert(80);
+    bst.insert(75);
+    bst.insert(90);
+    bst.insert(100);
+    bst.insert(77);
+
+    bst.remove(70);
 
     bst.inorder();
 
